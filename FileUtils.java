b4e456c3bfa04ed93c8097d6f6f290b1634b576f
flowstone <xueyao.me@gmail.com>
2017-11-28 20:21:41 +0800

    add 下载文件名乱码解决

--- FileUtils.java
+++ FileUtils.java
@@ -14,6 +14,8 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.net.URLEncoder;
+import sun.misc.BASE64Encoder;
 
 /**
  * 文件工具类
@@ -670,4 +672,29 @@ public abstract class FileUtils {
         
         return oldFile.renameTo(newFile);// 重命名   
     }
+	
+	/**
+	 * 下载文件时，针对不同浏览器，进行附件名的编码
+	 * @author : simon xue
+	 * @date : 2017年11月28日 下午19:58:05
+	 * @param filename
+	 *            下载文件名
+	 * @param agent
+	 *            客户端浏览器
+	 * @return 编码后的下载附件名
+	 * @throws IOException
+	 */
+	public static String encodeDownloadFilename(String filename, String agent)
+			throws IOException {
+		if (agent.contains("Firefox")) { // 火狐浏览器
+			filename = "=?UTF-8?B?"
+					+ new BASE64Encoder().encode(filename.getBytes("utf-8"))
+					+ "?=";
+			filename = filename.replaceAll("\r\n", "");
+		} else { // IE及其他浏览器
+			filename = URLEncoder.encode(filename, "utf-8");
+			filename = filename.replace("+"," ");
+		}
+		return filename;
+	}
 }
